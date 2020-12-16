31694251eefef3efc772d70fcba6f3e0b8ca05f7
angeles890 <a.angeles890@gmail.com>
2020-12-15 19:02:28 -0800

    Migration and initial web api code for new project (Demo notes)
    New web api portion of notes demo project

---
+++ notes-app/backend_web_api/Notes.db/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Notes.db
+{
+    public class Note
+    {
+        [Key]
+        public int id { get; set; }
+        [Column(TypeName = "nvarchar(max)")]
+        public string value { get; set; }
+    }
+}
